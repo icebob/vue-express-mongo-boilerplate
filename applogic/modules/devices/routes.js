9d26a75d25da637b7af8c8117cf904cb5efc5545
Icebob <mereg.norbert@gmail.com>
2016-06-27 10:46:28 +0200

    REST API sample as /device

--- applogic/modules/devices/routes.js
+++ applogic/modules/devices/routes.js
@@ -7,6 +7,8 @@ let express			= require("express");
 
 let auth			= require("../../../core/auth/helper");
 let Device 			= require("../../../models/devices");
+let hashids			= require("../../../libs/hashids");
+let response		= require("../../../libs/response");
 
 module.exports = function(app, db) {
 
@@ -20,14 +22,15 @@ module.exports = function(app, db) {
 		
 			Device.find({}).exec((err, docs) => {
 
-				if (docs.length == 0) return res.json([]);
-
-				let items = docs.map((item) => item.toJSON());
-
+				// Sample for multiple response by accept header
 				return res.format({
 					// html() { res.render("devices", { devices: items })}
 					json() {
-						res.json(items);
+						if (docs.length == 0) return response.json(res, []);
+
+						let items = docs.map((item) => item.toJSON());
+
+						return response.json(res, items);
 					}
 				});
 
@@ -40,11 +43,9 @@ module.exports = function(app, db) {
 			req.assert('name', 'Device name cannot be blank!').notEmpty();
 
 			let errors = req.validationErrors();
-			if (errors) {
-				return res.json({
-					error: errors
-				});
-			}
+			if (errors)
+				return response.json(res, null, response.BAD_REQUEST, errors);
+		
 
 			let device = new Device({
 				address: req.body.address,
@@ -55,15 +56,82 @@ module.exports = function(app, db) {
 
 			device.save((err) => {
 				if (err)
-					return res.json({
-						error: err
-					});
+					return response.json(res, null, response.SERVER_ERROR, err);
 
-				return res.json({
-					result: device.toJSON()
-				});
+				return response.json(res, device.toJSON());
+			});
+		});
+
+	// Resolve deviceID and device
+	router.param("deviceID", function(req, res, next, deviceID) {
+		let id = hashids.decodeHex(deviceID);
+		if (id == null || id == "")
+			return response.json(res, null, response.BAD_REQUEST, "Invalid Device ID!");
+
+		Device.findById(id, (err, doc) => {
+			if (err)
+				return response.json(res, null, response.BAD_REQUEST, err);
+			
+			if (!doc) 
+			return response.json(res, null, response.NOT_FOUND, "Device not found!");
+
+			req.device = doc;
+			next();
+		});
+	})
+
+	router.route("/:deviceID")
+
+		.all((req, res, next) => {
+
+			next();
+		})
+
+		// Get device
+		.get((req, res) => {
+			return response.json(res, req.device.toJSON());
+		})
+
+		// Modify device
+		.put((req, res) => {
+			if (req.body.address != null)
+				req.device.address = req.body.address;
+
+			if (req.body.type != null)
+				req.device.type = req.body.type;
+
+			if (req.body.name != null)
+				req.device.name = req.body.name;
+
+			if (req.body.description != null)
+				req.device.description = req.body.description;
+
+			if (req.body.status != null)
+				req.device.status = req.body.status;
+
+			req.device.save((err) => {
+				if (err)
+					return response.json(res, null, response.BAD_REQUEST, err);
+
+				return response.json(res, req.device.toJSON());
 			});
 		})
 
+		// Delete device
+		.delete((req, res) => {
+			Device.remove({ _id: req.device.id }, (err) => {
+				if (err)
+					return response.json(res, null, response.BAD_REQUEST, err);
+
+				return response.json(res);
+			});
+
+		});
+
+
+	// API versioning
+	app.use("/v1/devices", router);
+
+	// v1 is the default route
 	app.use("/devices", router);
 };
