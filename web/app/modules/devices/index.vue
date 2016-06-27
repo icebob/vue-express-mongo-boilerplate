09a2b34a50ada0a750fcdc90b9a18917091007bc
Icebob <mereg.norbert@gmail.com>
2016-06-27 16:43:25 +0200

    Implement VueFormGenerator

--- web/app/modules/devices/index.vue
+++ web/app/modules/devices/index.vue
@@ -13,7 +13,7 @@
 					th Last communication
 
 			tbody
-				tr(v-for="device in devices", @click="selectDevice(device)", :class="{ selected: device == selected }")
+				tr(v-for="device in devices", @click="select(device)", :class="{ selected: device == selected }")
 					td {{ device.code }}
 					td {{ device.address }}
 					td {{ device.name }}
@@ -21,22 +21,60 @@
 					td {{ device.status | status }}
 					td {{ device.lastCommunication | ago }}
 
+		.device-form
+			.errors.text-center
+				div.alert.alert-danger(v-for="item in validationErrors", track-by="$index") {{ item.field.label}}: 
+					strong {{ item.error }}
+
+			.buttons.text-center
+				button.btn.btn-default.new(@click="newModel") 
+					i.fa.fa-plus
+					| New
+				button.btn.btn-primary.save(@click="saveModel") 
+					i.fa.fa-floppy-o
+					| Save
+				button.btn.btn-danger.delete(@click="deleteModel") 
+					i.fa.fa-trash
+					| Delete
+
+			vue-form-generator(:schema='schema', :model='model', :options='formOptions', :multiple="false", v-ref:form, :is-new-model="isNewModel")
 
 </template>
 
 <script>
+	import Vue from "vue";
+	import VueFormGenerator from "vue-form-generator";
+	import Schema from "./schema";
+
+	import { cloneDeep } from "lodash";
+
 	import { getDevices, selectDevice } from "../../vuex/actions";
-	
 	import { devices, selected } from "../../vuex/getters";
 
+	// Initialize vue-form-generator
+	Vue.use(VueFormGenerator);
+
 	export default {
+
+		data() {
+			return {
+				schema: Schema,
+				model: null,
+				isNewModel: false,
+
+				formOptions: {
+					validateAfterLoad: true,
+					validateAfterChanged: false,
+					validateBeforeSave: true
+				}
+			}
+		},
 
 		vuex: {
 			getters: {
 				devices,
 				selected
 			},
-
 			actions: {
 				selectDevice
 			}
@@ -49,6 +87,69 @@
 				else
 					return "Inactive";
 			}
+		},	
+
+		computed: {
+			validationErrors() {
+				if (this.$refs.form && this.$refs.form.errors) 
+					return this.$refs.form.errors;
+
+				return [];
+			}
+		},	
+
+		methods: {
+			select(device) {
+				this.model = cloneDeep(device);
+
+				this.selectDevice(this.$store, device);
+			},
+
+			newModel() {
+				console.log("Create new model...");
+				this.selectDevice(null);
+
+				let newRow = VueFormGenerator.schema.createDefaultObject(Schema)
+				this.isNewModel = true;
+				this.model = newRow;
+
+				let el = document.querySelector("div.form input:nth-child(1):not([readonly]):not(:disabled)");
+				if (el)
+					el.focus()
+
+			},			
+
+			saveModel() {
+				console.log("Save model...");
+				if (this.formOptions.validateBeforeSave === false ||  this.validate()) {
+
+					if (this.isNewModel) {
+						this.$http.post("/devices", this.model).then((response) => {
+							let res = response.json();
+							// TODO add new device to store.device.all
+						});						
+					} else
+						this.$http.put("/devices/" + this.model.code, this.model).then((response) => {
+							let res = response.json();
+							// TODO refresh device properties from response
+						});
+
+				} else {
+					// Validation error
+				}
+			},
+
+			deleteModel() {
+				if (this.selected) {
+					this.$http.delete("/devices/" + this.model.code).then((response) => {
+						this.selectDevice(null);
+					});
+				}
+			},
+
+			validate()	{
+				return this.$refs.form.validate();
+			}			
 		},	
 
 		route: {
---
+++ web/app/modules/devices/schema.js
@@ -0,0 +1,66 @@
+import moment from "moment";
+import { validators } from "vue-form-generator";
+
+module.exports = {
+	fields: [
+		{
+			type: "text",
+			label: "ID",
+			model: "code",
+			readonly: true,
+			disabled: true
+		},
+		{
+			type: "select",
+			label: "Type",
+			model: "type",
+			required: true,
+			values: [
+				{ id: "rasperry", name: "Raspberry" },
+				{ id: "odroid", name: "ODroid" },
+				{ id: "nanopi", name: "NanoPI" },
+				{ id: "pc", name: "PC" }
+			],
+			default: "raspberry"
+		},	
+		{
+			type: "text",
+			label: "Name",
+			model: "name",
+			featured: true,
+			required: true,
+			placeholder: "Device name",
+			validator: validators.string
+		},
+		{
+			type: "text",
+			label: "Description",
+			model: "description",
+			featured: false,
+			required: false,
+			validator: validators.string
+		},	
+		{
+			type: "text",
+			label: "Address",
+			model: "address",
+			placeholder: "Address of device",
+			validator: validators.string
+		},
+		{
+			type: "label",
+			label: "Last communication",
+			model: "lastCommunication",
+			get(model) { return model && model.created ? moment(model.created).format("LLL") : "-"; }
+		},
+		{
+			type: "switch",
+			label: "Status",
+			model: "status",
+			multi: true,
+			default: true,
+			textOn: "Active",
+			textOff: "Inactive"
+		}
+	]
+}
