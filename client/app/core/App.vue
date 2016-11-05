be60d54949c71902e826790aa735d80c316c9626
Icebob <mereg.norbert@gmail.com>
2016-11-05 18:01:20 +0100

    add github ribbon

--- client/app/core/App.vue
+++ client/app/core/App.vue
@@ -8,7 +8,7 @@
 			router-view(keep-alive)
 
 	// a(href="https://github.com/icebob/vue-express-mongo-boilerplate")
-		img(style="position: absolute; top: 0; left: 0; border: 0;", src="https://camo.githubusercontent.com/8b6b8ccc6da3aa5722903da7b58eb5ab1081adee/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f6c6566745f6f72616e67655f6666373630302e706e67", alt="Fork me on GitHub", data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_left_orange_ff7600.png")
+		img(style="position: absolute; top: 0; left: 0; border: 0; z-index:2000", src="https://camo.githubusercontent.com/8b6b8ccc6da3aa5722903da7b58eb5ab1081adee/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f6c6566745f6f72616e67655f6666373630302e706e67", alt="Fork me on GitHub", data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_left_orange_ff7600.png")
 </template>
 
 <script>
