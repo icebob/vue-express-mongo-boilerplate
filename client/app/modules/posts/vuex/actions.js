00c29ac22727553e18d0854d8ff0bd7b04441d72
Icebob <mereg.norbert@gmail.com>
2016-11-16 16:47:12 +0100

    optimize posts actions

--- client/app/modules/posts/vuex/actions.js
+++ client/app/modules/posts/vuex/actions.js
@@ -8,12 +8,46 @@ let rest = new Service("posts").getRESTInterface();
 export const downloadRows = function ({ dispatch }, filter, sort) {
 	rest.invoke("find", { filter, sort }).then((data) => {
 		dispatch(LOAD, data);
+	}).catch((err) => {
+		toastr.error(err.message);
 	});
 };
 
-export const saveRow = function({ dispatch }, model) {
+export const saveRow = function(store, model) {
 	rest.invoke("save", model).then((data) => {
-		dispatch(ADD, data);
+		created(store, data);
+	}).catch((err) => {
+		toastr.error(err.message);
+	});
+};
+
+export const updateRow = function(store, model) {
+	rest.invoke("update", model).then((data) => {
+		updated(store, data);
+	}).catch((err) => {
+		toastr.error(err.message);
+	});
+};
+
+export const removeRow = function(store, model) {
+	rest.invoke("remove", model).then((data) => {
+		removed(store, data);
+	}).catch((err) => {
+		toastr.error(err.message);
+	});
+};
+
+export const upVote = function(store, model) {
+	rest.invoke("upVote", { code: model.code }).then((data) => {
+		updated(store, data);
+	}).catch((err) => {
+		toastr.error(err.message);
+	});
+};
+
+export const downVote = function(store, model) {
+	rest.invoke("downVote", { code: model.code }).then((data) => {
+		updated(store, data);
 	}).catch((err) => {
 		toastr.error(err.message);
 	});
@@ -23,43 +57,10 @@ export const created = function({ dispatch }, model) {
 	dispatch(ADD, model);
 };
 
-export const updateRow = function({ dispatch }, model) {
-	rest.invoke("update", model).then((data) => {
-		dispatch(UPDATE, data);
-	}).catch((err) => {
-		toastr.error(err.message);
-	});
-};
-
 export const updated = function({ dispatch }, model) {
 	dispatch(UPDATE, model);
 };
 
-export const removeRow = function({ dispatch }, model) {
-	rest.invoke("remove", model).then((data) => {
-		dispatch(REMOVE, data);
-	}).catch((err) => {
-		toastr.error(err.message);
-	});
-};
-
 export const removed = function({ dispatch }, model) {
 	dispatch(REMOVE, model);
-};
-
-
-export const upVote = function({ dispatch }, model) {
-	rest.invoke("upVote", { code: model.code }).then((data) => {
-		dispatch(UPDATE, data);
-	}).catch((err) => {
-		toastr.error(err.message);
-	});
-};
-
-export const downVote = function({ dispatch }, model) {
-	rest.invoke("downVote", { code: model.code }).then((data) => {
-		dispatch(UPDATE, data);
-	}).catch((err) => {
-		toastr.error(err.message);
-	});
 };
